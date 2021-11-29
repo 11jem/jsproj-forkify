0a0b47436503d6f1d66fc7101a8c57c8e7f93bf0
11jem <cabalidajem@gmail.com>
2021-11-29 01:11:40 -0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5f2e8fe9-4a9f-4a31-b52d-cbb823f7c4a5';
+export const MODAL_CLOSE_SEC = 2.5;
